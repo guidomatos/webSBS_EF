29377240971eb8ad5c79c956152326eb78d2014e
guidomatos <guido.matos.88@gmail.com>
2020-12-21 21:40:14 -0500

    Se crea arquitectura limpia

---
+++ SBS.Utils/Constantes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SBS.Utils
+{
+    public static class Constantes
+    {
+        public const string FormatoFechaES = "dd/MM/yyyy";
+        public const string FormatoFechaEN = "yyyy-MM-dd";
+    }
+}
---
+++ SBS.Utils/Funciones.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SBS.Utils
+{
+    public class Funciones
+    {
+        public static bool ValidarFecha(string fechaValidar, out DateTime fecha)
+        {
+            fecha = new DateTime();
+            var arrayFecha = fechaValidar.Split('/');
+
+            if (arrayFecha.Length == 3)
+            {
+                bool resultado;
+
+                resultado = Int32.TryParse(arrayFecha[0], out _);
+                if (!resultado) return false;
+
+                resultado = Int32.TryParse(arrayFecha[1], out _);
+                if (!resultado) return false;
+
+                resultado = Int32.TryParse(arrayFecha[2], out _);
+                if (!resultado) return false;
+
+                var dia = Convert.ToInt32(arrayFecha[0]);
+                var mes = Convert.ToInt32(arrayFecha[1]);
+                var anio = Convert.ToInt32(arrayFecha[2]);
+
+                try
+                {
+                    fecha = new DateTime(anio, mes, dia);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
